4517eb1fb695b6faf33c091176796eabfc9beeed
suplookie <hsk0306@kaist.ac.kr>
2019-12-03 22:36:45 +0900

    select only tutee access/moreinfo register, delete grade table/signup add freshman for department

--- matcher/moreInfo.php
+++ matcher/moreInfo.php
@@ -40,50 +40,69 @@
     echo "<FORM METHOD=\"post\" ACTION=\"moreInfo.php\">";
 
     if (isset($_POST["name"])) {   //from sign up
-        $_SESSION['studNum'] = $_POST["StudNum"];
-        $_SESSION['password'] = $_POST['password'];
-        echo "<TABLE CELLPADDING = \"5\" CELLSPACING = \"1\" border='1'>";
-        echo "<TR><TD align='right'>Name &nbsp;</TD><TD><input type='text' name='name' value=". $_POST["name"]. "></TD>";
-        echo "<TR><TD align='right'>Password &nbsp;</TD><TD><input type='password' name='password'></TD></TR>";
-        echo "<TR><TD align='right'>Student Number &nbsp;</TD><TD>". $_POST["StudNum"]. "</TD>";
-        echo "<TR><TD align='right'>Department &nbsp;</TD><TD><select name=\"department\">
-                        <option value=". $_POST["department"]. ">". $_POST["department"]. "</option>
-                        <option value=\"PH\">Physics</option>
-                        <option value=\"MAS\">Mathematics</option>
-                        <option value=\"CH\">Chemistry</option>
-                        <option value=\"BS\">Biology</option>
-                        <option value=\"CS\">Computer Science</option>
-                        <option value=\"EE\">Electronic Engineering</option>
-                        <option value=\"CE\">Civil & Environment Engineering</option>
-                        <option value=\"BiS\">Bio & Brain Engineering</option>
-                        <option value=\"ID\">Industrial Design</option>
-                        <option value=\"IE\">Industrial & System Engineering</option>
-                        <option value=\"CBE\">Chemical & Biomolecular Engineering</option>
-                        <option value=\"NQE\">Nuclear & Quantum Engineering</option>
-                        <option value=\"MS\">Material Science And Engineering</option>
-                        <option value=\"ME\">Mechanical Engineering</option>
-                        <option value=\"AE\">Aerospace Engineering</option>
-                        <option value=\"MSB\">Business</option>
-                    </select></TD></TR>";
-        echo "<TR><TD align='right'>Email &nbsp;</TD><TD><input type='text' name='email' value=". $_POST["email"]. "></TD>";
-        if(isset($_POST["tutor"])) {
-            echo "<TR><TD align='right' rowspan='3'>Add Grade&nbsp;</TD><TD><input type='text' name='course' placeholder='course'></TD></TR>";
-            echo "<TR><TD><input type='text' name='prof' placeholder='professor'></TD></TR>";
-            echo "<TR><TD><input type='text' name='grade' placeholder='grade'></TD></TR>";
+        $checkDup = $conn->query("select * from match.kaist_stu where stu_num = ". $_POST['StudNum']);
+        if ($checkDup -> num_rows > 0)
+            echo "Student ID is already registered. ";
+        else {
+            $kaist = "insert into match.kaist_stu (stu_num, name, department, email, password) values (". $_POST["StudNum"]. ", '". $_POST["name"]. "', '". $_POST["department"]. "', '". $_POST["email"]. "', '". $_POST["password"]. "');";
+            echo $kaist;
+            $conn->query($kaist);
+            if ($_POST["tt"] == "tutor") {
+                echo "tutor";
+                $conn->query("insert into match.tutor (stuNum, avgrade) values (". $_POST["StudNum"]. ", '0');");
+            }
+            else{
+                echo "tutee";
+                $conn->query("insert into match.tutee (stunum) values (". $_POST["StudNum"]. ");");
+            }
+            $_SESSION['studNum'] = $_POST["StudNum"];
+            $_SESSION['password'] = $_POST['password'];
+            echo "<TABLE CELLPADDING = \"5\" CELLSPACING = \"1\" border='1'>";
+            echo "<TR><TD align='right'>Name &nbsp;</TD><TD><input type='text' name='name' value=". $_POST["name"]. "></TD>";
+            echo "<TR><TD align='right'>Password &nbsp;</TD><TD><input type='password' name='password'></TD></TR>";
+            echo "<TR><TD align='right'>Student Number &nbsp;</TD><TD>". $_POST["StudNum"]. "</TD>";
+            echo "<TR><TD align='right'>Department &nbsp;</TD>
+                <TD><select name=\"department\">
+                    <option value=". $_POST["department"]. ">". $_POST["department"]. "</option>
+                    <option value=\"PH\">Physics</option>
+                    <option value=\"MAS\">Mathematics</option>
+                    <option value=\"CH\">Chemistry</option>
+                    <option value=\"BS\">Biology</option>
+                    <option value=\"CS\">Computer Science</option>
+                    <option value=\"EE\">Electronic Engineering</option>
+                    <option value=\"CE\">Civil & Environment Engineering</option>
+                    <option value=\"BiS\">Bio & Brain Engineering</option>
+                    <option value=\"ID\">Industrial Design</option>
+                    <option value=\"IE\">Industrial & System Engineering</option>
+                    <option value=\"CBE\">Chemical & Biomolecular Engineering</option>
+                    <option value=\"NQE\">Nuclear & Quantum Engineering</option>
+                    <option value=\"MS\">Material Science And Engineering</option>
+                    <option value=\"ME\">Mechanical Engineering</option>
+                    <option value=\"AE\">Aerospace Engineering</option>
+                    <option value=\"MSB\">Business</option>
+                    <option value=\"Fresh\">Freshman</option>
+                 </select></TD></TR>";
+            echo "<TR><TD align='right'>Email &nbsp;</TD><TD><input type='text' name='email' value=". $_POST["email"]. "></TD>";
+            /*if(isset($_POST["tutor"])) {
+                echo "<TR><TD align='right' rowspan='3'>Add Grade&nbsp;</TD><TD><input type='text' name='course' placeholder='course'></TD></TR>";
+                echo "<TR><TD><input type='text' name='prof' placeholder='professor'></TD></TR>";
+                echo "<TR><TD><input type='text' name='grade' placeholder='grade'></TD></TR>";
+            }*/
+            echo "<TR><TD colspan='2' align='center'><p><INPUT type=\"submit\" value=\"update\">&nbsp;<INPUT type=\"reset\" value='clear'></p></TD></TR>";
+            echo "</TABLE>";
+            echo "</FORM>";
         }
-        echo "<TR><TD colspan='2' align='center'><p><INPUT type=\"submit\" value=\"update\">&nbsp;<INPUT type=\"reset\" value='clear'></p></TD></TR>";
-        echo "</TABLE>";
-        echo "</FORM>";
+
     }
 
     else if (isset($_POST["StudNum"]) || isset($_SESSION['studNum'])) { //from sign in
-        if (isset($_POST["course"])&& isset($_POST["prof"]) && isset($_POST["grade"])) {        //update tutor grade
+        /*if (isset($_POST["course"])&& isset($_POST["prof"]) && isset($_POST["grade"])) {        //update tutor grade
             $qu = "select * from match.kaistcourses where course='". $_POST["course"]. "' and prof= '". $_POST["prof"]. "'";
             $courseCon = $conn->query($qu);
             $course=$courseCon->fetch_assoc();
             $query = "insert into match.tutorgrade (stunum, grade, courseId) values (". $_POST["StudNum"]. ", '". $_POST["grade"]. "', ". $course["idkaistCourses"]. ")";
             $conn->query($query);
-        }
+        }*/
 
         if (isset($_SESSION['studNum'])) {
             $selection = "select * from match.kaist_stu where stu_num = ". $_SESSION["studNum"]. " and password = ".$_SESSION["password"];
@@ -120,20 +139,21 @@
                     <option value=\"ME\">Mechanical Engineering</option>
                     <option value=\"AE\">Aerospace Engineering</option>
                     <option value=\"MSB\">Business</option>
+                    <option value=\"Fresh\">Freshman</option>
                 </select></TD></TR>";
             echo "<TR><TD align='right'>Email &nbsp;</TD><TD><input type='text' name='email' value=". $now["email"]. "></TD></TR>";
             $tutorCheck = "select * from match.tutor where stuNum=". $_POST["StudNum"];
             $tutorRes = $conn -> query($tutorCheck);
-            if ($tutorRes -> num_rows > 0) {        //if tutor, add table for upgrading grade
+            /*if ($tutorRes -> num_rows > 0) {        //if tutor, add table for upgrading grade
                 //$tutor = $tutorRes -> fetch_assoc();
                 echo "<TR><TD align='right' rowspan='3'>Add Grade&nbsp;</TD><TD><input type='text' name='course' placeholder='course'></TD></TR>";
                 echo "<TR><TD><input type='text' name='prof' placeholder='professor'></TD></TR>";
                 echo "<TR><TD><input type='text' name='grade' placeholder='grade'></TD></TR>";
-            }
+            }*/
             echo "<TR><TD colspan='2' align='center'><p><INPUT type=\"submit\" value=\"update\">&nbsp;<INPUT type=\"reset\" value='clear'></p></TD></TR>";
             echo "</TABLE>";
             echo "</FORM>";
-
+/*
             if ($tutorRes -> num_rows > 0) {        //if tutor, show my grade I uploaded
                 $getGrade = $conn -> query("select * from match.tutorgrade where stunum=". $_POST["StudNum"]);
                 echo "<TABLE CELLPADDING = \"5\" CELLSPACING = \"1\">";
@@ -144,7 +164,7 @@
                     echo "<TR><TD align='center'>". $course["course"]. "</TD><TD align='center'>". $course["prof"]. "</TD><TD align='center'>". $grade["grade"]. "</TD></TR>";
                 }
                 echo "</TABLE>";
-            }
+            }*/
         }
         else {          //login fail
             echo "Student ID or password is wrong. <br></FORM>";
--- matcher/select.php
+++ matcher/select.php
@@ -25,7 +25,7 @@ $servername = "143.248.219.83";
 $username = "test";
 $password = "1234";
 $dbname = "match";
-
+session_start();
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -33,42 +33,50 @@ if ($conn-> connect_error) {
     die("Connection failed: " + $conn -> connect_error);
 }
 
-echo "<FORM METHOD=\"post\" ACTION=\"match.php\">";
-
-$selection = "select * from match.open_class where idopen_class not in 
-                    (select classId from match.tutoring_match)";
-//모든 오픈클래스가 아닌, tutoring match에 없는 클래스만 가져오는거로 바꾸기
-$result = $conn -> query($selection);
-echo "<h4>Select class you want. If not, add to your wishlist</h4>";
-if ($result -> num_rows > 0) {
-    echo "<TABLE cellpadding='5' cellspacing='1' border='1'>";
-    echo "<TR><TD></TD></TD><TD>Course</TD><TD>Professor</TD><TD>TutorNum</TD><TD>Tutor Name</TD><TD>Tutor Department</TD><TD>Price</TD></TR>";
-    while ($now = $result -> fetch_assoc()) {
-        $classid = $now["idopen_class"];
-        $courseCon = $conn -> query("select * from match.kaistcourses where idkaistCourses=".$now["courseId"]);
-        $course = $courseCon -> fetch_assoc();
-        $tutorCon = $conn -> query("select * from match.kaist_stu where stu_num=". $now["tutorNum"]);
-        $tutor = $tutorCon ->fetch_assoc();
-        echo "<TR><TD><input type='radio' name='class' value=". $classid. "></TD>";
-        echo "<TD>". $course["course"]. "</TD>";
-        echo "<TD>". $course["prof"]. "</TD>";
-        echo "<TD>". $tutor["stu_num"]. "</TD>";
-        echo "<TD>". $tutor["name"]. "</TD>";
-        echo "<TD>". $tutor["department"]. "</TD>";
-        echo "<TD>". $now["price"]. "</TD>";
-        echo "</TR>";
-    }
-    echo "</TABLE>";
-}
+if (!isset($_SESSION['studNum']))
+    echo "<h4>please Sign In</h4>";
 else {
-    echo "<h1>Warning: NO CLASS OPENED</h1>";
+    $tuteeCheck = $conn -> query("select * from match.tutee where stunum = ". $_SESSION['studNum']);
+    if ($tuteeCheck -> num_rows > 0) {
+        echo "<FORM METHOD=\"post\" ACTION=\"match.php\">";
+
+        $selection = "select * from match.open_class where idopen_class not in 
+                        (select classId from match.tutoring_match)";
+        //모든 오픈클래스가 아닌, tutoring match에 없는 클래스만 가져오는거로 바꾸기
+        $result = $conn -> query($selection);
+        echo "<h4>Select class you want. If not, add to your wishlist</h4>";
+        if ($result -> num_rows > 0) {
+            echo "<TABLE cellpadding='5' cellspacing='1' border='1'>";
+            echo "<TR><TD></TD></TD><TD>Course</TD><TD>Professor</TD><TD>TutorNum</TD><TD>Tutor Name</TD><TD>Tutor Department</TD><TD>Price</TD></TR>";
+            while ($now = $result -> fetch_assoc()) {
+                $classid = $now["idopen_class"];
+                $courseCon = $conn -> query("select * from match.kaistcourses where idkaistCourses=".$now["courseId"]);
+                $course = $courseCon -> fetch_assoc();
+                $tutorCon = $conn -> query("select * from match.kaist_stu where stu_num=". $now["tutorNum"]);
+                $tutor = $tutorCon ->fetch_assoc();
+                echo "<TR><TD><input type='radio' name='class' value=". $classid. "></TD>";
+                echo "<TD>". $course["course"]. "</TD>";
+                echo "<TD>". $course["prof"]. "</TD>";
+                echo "<TD>". $tutor["stu_num"]. "</TD>";
+                echo "<TD>". $tutor["name"]. "</TD>";
+                echo "<TD>". $tutor["department"]. "</TD>";
+                echo "<TD>". $now["price"]. "</TD>";
+                echo "</TR>";
+            }
+            echo "</TABLE>";
+        }
+        else {
+            echo "<h1>Warning: NO CLASS OPENED</h1>";
+        }
+        echo "<p><INPUT type=\"submit\" value=\"Submit\"> &nbsp;";
+        echo "<button type=\"button\" onclick=\"location.href ='http://localhost:8080/matcher/mywish.php' \"> Add wishlist </button></p>";
+        echo "</FORM>";
+    }
+    else
+        echo "Only tutee can select class";
+
 }
-echo "<p><INPUT type=\"submit\" value=\"Submit\"> &nbsp;";
-echo "<button type=\"button\" onclick=\"location.href ='http://localhost:8080/matcher/mywish.php' \"> Add wishlist </button></p>";
-echo "</FORM>";
-
 $conn -> close();
-
 ?>
 
 </html>

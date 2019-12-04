b2be798c959571dbbe2d754c3f4bad52c4137564
suplookie <hsk0306@kaist.ac.kr>
2019-12-04 13:06:47 +0900

    tutorinfo

--- matcher/tutorInfo.php
+++ matcher/tutorInfo.php
@@ -62,11 +62,21 @@ if (isset($_GET["courseId"])) {
     $q = "select * from match.tuteegrade where tutorNum=". $_GET["tutor"]. ";";
     echo "<br><div id='please' align='center'>tutee's evalation</div>";
     echo "<TABLE border=\"0\" width=\"400\" CELLPADDING = \"5\" CELLSPACING = \"1\" align=\"center\">";
-    echo "<TR></TD><TD>Grade</TD><TD>Satisfaction</TD><TD>Comment</TD></TR>";
+    echo "<TR></TD><TD>Grade</TD><TD>Satisfaction</TD><TD>Comment</TD><TD>Semester</TD></TR>";
     $tuteeGrade = $conn->query($q);
     if ($tuteeGrade->num_rows > 0) {
-        while ($tuteeNow = $tuteeGrade->fetch_assoc()) {
-            echo "<TR><TD>". $tuteeNow["grade"]. "</TD><TD>". $tuteeNow["satisfy"]. "</TD><TD>". $tuteeNow["comment"]. "</TD></TR>";
+        while ($tuteeNow = $tuteeGrade->fetch_assoc()) {        //tuteeNow: 어떤 튜터에 대한 tuteegrade 1개
+            $matchId = $tuteeNow["classId"];            //그 tuteegrade에 연결된, tutee가 받은 수업의 matchId
+            $que = "select * from match.tutoring_match where idtutoring_match = ". $matchId. ";";
+            $match = $conn->query($que);
+            $matchNow = $match->fetch_assoc();
+            $classId = $matchNow["classId"];            //tuteegrade에 연결된, 수업의 classid
+            $qu = "select * from match.open_class where idopen_class=". $classId. ";";
+            $opens = $conn->query($qu);
+            if($opens->num_rows > 0)
+                echo "<TR><TD>". $tuteeNow["grade"]. "</TD><TD>". $tuteeNow["satisfy"]. "</TD><TD>". $tuteeNow["comment"]. "</TD><TD>". $matchNow["semester"]. "</TD></TR>";
+            //else
+                //echo "<TR><TD>$que</TD><TD>$qu</TD></TR>";
         }
     }
 
